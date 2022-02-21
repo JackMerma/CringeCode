6c1e8853d07ab914ad77ad5c1df55d25256f72e2
Jackson Merma <jmermap@unsa.edu.pe>
2022-02-21 14:17:12 -0500

    Leida de una cadena o entrada en general

--- prueba/Prueba.java
+++ prueba/Prueba.java
@@ -3,9 +3,15 @@
  * @created     : 21/02/2022
  * @filename    : Prueba
  */
+
+import java.util.Scanner;
+
 class Prueba {
 	public static void main(String [] args){
-		System.out.println("Hola mundo");
+		Scanner scan = new Scanner(System.in);
+		String name = scan.next();
+
+		System.out.println("Hola " + name);
 	}
 }
 
