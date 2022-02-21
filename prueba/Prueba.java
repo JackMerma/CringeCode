fe05804c182169df509e2d011fd12698ac5b1a84
Jackson Merma <jmermap@unsa.edu.pe>
2022-02-21 13:59:34 -0500

    prueba simple de archivo sin entrada de datos

---
+++ prueba/Prueba.java
@@ -0,0 +1,12 @@
+/**
+ * @author      : Jackson Merma (mermaportocarreroj@gmail.com)
+ * @created     : 21/02/2022
+ * @filename    : Prueba
+ */
+class Prueba {
+	public static void main(String [] args){
+		System.out.println("Hola mundo");
+	}
+}
+
+
