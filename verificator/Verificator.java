98db61d36db7334322cab445801bda761933b798
Jackson Merma <jmermap@unsa.edu.pe>
2022-02-22 11:33:56 -0500

    plantilla de verificacion

---
+++ verificator/Verificator.java
@@ -0,0 +1,40 @@
+/**
+ * @author      : Jackson Merma (mermaportocarreroj@gmail.com)
+ * @created     : 22/02/2022
+ * @filename    : Verificator
+ */
+class Verificator {
+	public Verificator(){
+		//
+	}
+
+	private int getCases(String code){
+		//
+		return 10;
+	}
+
+	private String getOutputUser(String code){
+
+	}
+	private String getOutputBot(String code){
+
+	}
+
+	public void verificate(String code){
+		//
+		// cantidad de casos mediante un archivo
+		int cases = getCases(code);
+
+
+		//	salidas de usuario y esperadas
+		String outputBot, outputUser;
+
+		outputBot = getOutputBot(code);
+		outputUser = getOutputUser(code);
+	}
+	public static void main(String [] args){
+		//code
+	}
+}
+
+
