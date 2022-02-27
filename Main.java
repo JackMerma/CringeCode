d8a30851ccc2840e44b4c657b4348d0e96b97e1e
Jack Merma <jmermap@unsa.edu.pe>
2022-02-26 21:35:42 -0500

    primera prueba en un main con una solucion implentada de codigo NTN

---
+++ Main.java
@@ -0,0 +1,23 @@
+/** 
+* @author      : Jackson Fernando Merma Portocarrero (jmermap@unsa.edu.pe)
+* @created     : 26/02/2022
+* @filename    : Main
+* @description : _
+*/
+
+import java.util.*;
+import sources.Objects.*;
+
+public class Main{
+	public static void main(String[]args){
+		Problem pro1 = new Problem("NTN");//n times name
+		pro1.setInput("2\nJackson\nFernando\n");
+
+		Writer<Problem> wri = new Writer<Problem>("sources/files/problems");
+		wri.write(pro1, pro1.getCode());
+
+		Maker maker = new Maker();
+		maker.execute(pro1.getCode());
+	}
+}
+
