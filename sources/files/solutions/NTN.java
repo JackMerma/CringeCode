cb927cf1b67a73ebaf756c73223ebba77096d2c3
Jack Merma <jmermap@unsa.edu.pe>
2022-02-26 22:14:31 -0500

    ficheros generados con el ejemplo

---
+++ sources/files/solutions/NTN.java
@@ -0,0 +1,22 @@
+/** 
+* @author      : Jackson Fernando Merma Portocarrero (jmermap@unsa.edu.pe)
+* @created     : 26/02/2022
+* @filename    : solution_NTN
+* @description : _
+*/
+
+import java.util.*;
+
+public class NTN{
+	public static void main(String[]args){
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+
+		for(int i=0;i<n;i++){
+			String name = scan.next();
+			System.out.println("hello "+name);
+		}
+	}
+}
+
