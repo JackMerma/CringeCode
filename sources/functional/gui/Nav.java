129caac1f97a0a1de31a56f625fb124b3426594e
Jack Merma <jmermap@unsa.edu.pe>
2022-02-27 19:31:57 -0500

    barra de navegacion simple

--- sources/functional/gui/Nav.java
+++ sources/functional/gui/Nav.java
@@ -5,6 +5,8 @@
 * @description : _
 */
 
+package sources.functional.gui;
+
 import java.util.*;
 import java.awt.*;
 import javax.swing.*;
@@ -18,7 +20,7 @@ public class Nav{
 		JPanel content = getContent();
 
 		global.add(content);
-		//bordes...
+		createBorders(global);
 
 		return global;
 	}
@@ -37,11 +39,57 @@ public class Nav{
 	}
 
 	private JPanel getSuperPanel(){
+		JPanel global = new JPanel();
+		global.setLayout(new BorderLayout());
 
+		JPanel principal = new JPanel();
+		principal.setLayout(new BorderLayout());
+
+		//logo
+		JLabel logo = new JLabel("CRINGEcode");
+
+
+		
+		
+		principal.add(logo, BorderLayout.WEST);
+		global.add(principal);
+		createBorders(global);
+
+		return global;
 	}
 
 	private JPanel getUpperPanel(){
+		JPanel global = new JPanel();
+		global.setLayout(new BorderLayout());
 
+		JPanel principal = new JPanel();
+		principal.setLayout(new GridLayout(1,5));
+
+		//barra de menu
+		JButton home = new JButton("Home");
+		JButton news = new JButton("News");
+		JButton problems = new JButton("Problems");
+		JButton help = new JButton("Help");
+		JButton submissions = new JButton("Submissions");
+
+		principal.add(home);
+		principal.add(news);
+		principal.add(problems);
+		principal.add(help);
+		principal.add(submissions);
+
+
+		global.add(principal);
+		createBorders(global);
+
+		return global;
+	}
+
+	private void createBorders(JPanel panel){
+		panel.add(new JLabel(""), BorderLayout.NORTH);
+		panel.add(new JLabel(""), BorderLayout.SOUTH);
+		panel.add(new JLabel("   "), BorderLayout.EAST);
+		panel.add(new JLabel("   "), BorderLayout.WEST);
 	}
 }
 
